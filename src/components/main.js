bd0ac190170685cd2741e1eb27a85aded2c00919
user <user@users-iMac.local>
2019-03-02 16:45:39 +0000

    update code for getting geolocation

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-import {geolocated} from 'react-geolocated';
 import styled from 'styled-components'
 import axios from 'axios';
 
-let callAPI;
+
 const Container = styled.div`
     padding: 4em;
     text-align: center;
@@ -16,64 +15,84 @@ class Main extends Component {
         super();
 
         this.state = {
-            lat: '',
-            long: '',
+            statusText: '',
+            lat: 0,
+            long: 0,
+            town: '',
+            temp: [],
+            weather: [],
+            foundLocation: false,
         }
         
     }
 
-  ç
-    componentWillMount(){
+    componentDidMount(){
 
-        callAPI = (state) => {
-            const {long, lat} = state;
-            axios.get("https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long)
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
+        // let options = {
+        //     enableHighAccuracy: true,
+        //     timeout: 5000,
+        //   };
+          
+          const success = (pos) => {
+            let crd = pos.coords;
+
+            this.setState({
+                foundLocation: true,
+                long: crd.longitude,
+                lat: crd.latitude
             });
+            
+            this.setState({
+                statusText: `Successfully found you at ${this.state.lat},${this.state.long}`
+            });
+
+
+            const {long, lat} = this.state;
+            axios.get("https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+          }
+          
+           const error = (err) => {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+          }
+
+        if (!navigator.geolocation){
+            this.setState(
+                {statusText: 'Your browser does not support geolocation...'}
+                );
+        } else {
+            navigator.geolocation.getCurrentPosition(success, error, {timeout: 5000,});
         }
+
+
+            
+
+        } // End of componentdidmount
         
-    }
-
-    getLocation = () => {
-        this.setState({
-            lat: this.props.coords.latitude,
-            long: this.props.coords.longitude
-        })
-        console.log(this.state);
-    }
-
-    getWeather = () => {
-      callAPI(this.state);
-    }
     
-
   render() {
+        let weatherText;
 
-        return !this.props.isGeolocationAvailable
-        ? <div>Your browser does not support Geolocation</div>
-        : !this.props.isGeolocationEnabled
-          ? <div>Geolocation is not enabled</div>
-          : this.props.coords
-            ? <Container>
-                <table>
-                <tbody>
-                <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
-                <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
-                </tbody>
-                </table>
-            <button onClick={this.getLocation}> Get Location </button>
-            <button onClick={this.getWeather}>Get My Weather </button>
-            </Container>
-            : <div>Getting the location data&hellip; </div>;
+        if(this.state.foundLocation) {
+            weatherText = this.state.statusText;
+        } else {
+            weatherText = this.state.statusText;
+        }
+
+      return (
+        <Container>
+            <div className="weatherApp">
+            <h2>Weather info will display here</h2>
+            <p>{weatherText}</p>
+            </div>
+        </Container>  
+      )
+         
   }
 }
-export default geolocated({
-    positionOptions: {
-      enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-  })(Main);
+export default Main;
